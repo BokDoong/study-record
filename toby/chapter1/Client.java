9f4caa73e614bd06747f2ea59e7af2d19f4a6399
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-12-27 09:26:36 +0900

    Refactor UserDao initialization with Spring context

--- toby/chapter1/Client.java
+++ toby/chapter1/Client.java
@@ -4,7 +4,8 @@ public class Client {
 
     public static void main(String[] args) {
 
-        UserDao userDao = UserDaoFactory.createUserDao();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(UserDaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User("이복둥", 28);
         userDao.insertUser(user);
